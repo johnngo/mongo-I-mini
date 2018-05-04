a4f5c14281d1ee99e9db05e125fc37669b13c6a7
John <johnngo@Johns-MacBook-Pro.local>
2018-05-04 13:40:18 -0700

    added json packages

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const bearController = require('./bears/bearController');
 
@@ -18,5 +19,5 @@ server.use('/api/bears', bearController);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  console.log(`\n=== API running on http://localhost:${port} ===\n`);
+  console.log(`\n \n=== API running on http://localhost:${port} ===\n`);
 });
